de92f8dc79583dd34cea19fd1ad99b539c0cb5e3
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2023-10-09 14:08:06 +0300

    add mobile part

--- js/swipers.js
+++ js/swipers.js
@@ -1,3 +1,5 @@
+let mainSwiper = document.getElementsByClassName("swiper");
+
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   centeredSlides: true,
@@ -18,8 +20,26 @@ const navSwiper = new Swiper(".swipernav", {
   spaceBetween: 10,
   centeredSlides: true,
   slideToClickedSlide: true,
-  slidesPerView: 5,
+  slidesPerView: 3.5,
   speed: 1000,
+  breakpoints: {
+    1000: {
+      slidesPerView: 4.5,
+    },
+    1479: {
+      slidesPerView: 5,
+    },
+  },
 });
 swiper.controller.control = navSwiper;
 navSwiper.controller.control = swiper;
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth <= 768) {
+    navSwiper.destroy();
+  }
+});
+
+if (window.innerWidth >= 768) {
+  mainSwiper.remove();
+}
